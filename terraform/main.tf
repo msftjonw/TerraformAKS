636aaa9ad68e805cb4222e979c6544e019cef33e
msftjonw <35560783+msftjonw@users.noreply.github.com>
2021-11-17 21:29:37 -0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,69 +1,60 @@
-provider "azurerm" {
-  version = "~>2.0"
-  features {}
-}
-
-resource "azurerm_resource_group" "demo" {
-  name     = "${var.prefix}-rg"
-  location = var.location
-}
-
-resource "azurerm_virtual_network" "demo" {
-  name                = "${var.prefix}-network"
-  location            = azurerm_resource_group.demo.location
-  resource_group_name = azurerm_resource_group.demo.name
-  address_space       = ["10.1.0.0/16"]
-}
-
-resource "azurerm_subnet" "demo" {
-  name                 = "${var.prefix}-subnet"
-  virtual_network_name = azurerm_virtual_network.demo.name
-  resource_group_name  = azurerm_resource_group.demo.name
-  address_prefixes     = ["10.1.0.0/22"]
-}
-
-resource "azurerm_kubernetes_cluster" "demo" {
-  name                = "${var.prefix}-aks"
-  location            = azurerm_resource_group.demo.location
-  resource_group_name = azurerm_resource_group.demo.name
-  dns_prefix          = "${var.prefix}-aks"
-
-  default_node_pool {
-    name                = "default"
-    node_count          = 2
-    vm_size             = "Standard_D2_v2"
-    type                = "VirtualMachineScaleSets"
-    availability_zones  = ["1", "2"]
-    enable_auto_scaling = true
-    min_count           = 2
-    max_count           = 4
-
-    # Required for advanced networking
-    vnet_subnet_id = azurerm_subnet.demo.id
+terraform {
+  
+  required_providers {
+    azurerm = "2.52.0"
   }
 
-  identity {
-    type = "SystemAssigned"
+  resource "azurerm_resource_group" "rg" {
+    name     = "RG-AzureDevOps2"
+    location = var.location
   }
 
-  role_based_access_control {
-    azure_active_directory {
-      client_app_id     = var.client_app_id
-      server_app_id     = var.server_app_id
-      server_app_secret = var.server_app_secret
-      tenant_id         = var.tenant_id
+  resource "azurerm_virtual_network" "vnet" {
+    name                = "VNet-AzureDevOps2"
+    location            = azurerm_resource_group.demo.location
+    resource_group_name = azurerm_resource_group.demo.name
+    address_space       = ["10.0.0.0/8"]
+  }
+
+  resource "azurerm_subnet" "subnet" {
+    name                 = "Subnet-AzureDevOps2"
+    virtual_network_name = azurerm_virtual_network.demo.name
+    resource_group_name  = azurerm_resource_group.demo.name
+    address_prefixes     = ["10.0.0.0/16"]
+  }
+
+  resource "azurerm_kubernetes_cluster" "aks" {
+    name                = "AKS-AzureDevOps2"
+    location            = azurerm_resource_group.demo.location
+    resource_group_name = azurerm_resource_group.demo.name
+    dns_prefix          = "dns-azuredevops2"
+
+    default_node_pool {
+      name                = "systempool"
+      node_count          = 2
+      vm_size             = "Standard_DS2_v2"
+      type                = "VirtualMachineScaleSets"
+      availability_zones  = ["1", "2"]
+      enable_auto_scaling = true
+      min_count           = 2
+      max_count           = 4
+
+      # Required for advanced networking
+      vnet_subnet_id = azurerm_subnet.subnet.id
     }
-    enabled = true
+
+    identity {
+      type = "SystemAssigned"
+    }
+
+    network_profile {
+      network_plugin    = "azure"
+      load_balancer_sku = "standard"
+    }
+
+    tags = {
+      Environment = "azuredevops2"
+    }
   }
 
-  network_profile {
-    network_plugin    = "azure"
-    load_balancer_sku = "standard"
-    network_policy    = "calico"
-  }
-
-  tags = {
-    Environment = "Development"
-  }
 }
-
