a5ac21db122781ae39ef5b4b2026ce4d6223a858
msftjonw <35560783+msftjonw@users.noreply.github.com>
2021-11-17 22:03:41 -0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,27 +20,27 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg" {
   name     = "RG-AzureDevOps2"
-  location = var.location
+  location = "West US 2"
 }
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "VNet-AzureDevOps2"
-  location            = azurerm_resource_group.demo.location
-  resource_group_name = azurerm_resource_group.demo.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/8"]
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = "Subnet-AzureDevOps2"
-  virtual_network_name = azurerm_virtual_network.demo.name
-  resource_group_name  = azurerm_resource_group.demo.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.vnet.name
   address_prefixes     = ["10.0.0.0/16"]
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "AKS-AzureDevOps2"
-  location            = azurerm_resource_group.demo.location
-  resource_group_name = azurerm_resource_group.demo.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "dns-azuredevops2"
 
   default_node_pool {
