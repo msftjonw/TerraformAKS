93b2cdf7ecf6539c51690ad11da24438650714a3
msftjonw <35560783+msftjonw@users.noreply.github.com>
2021-11-17 22:18:58 -0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,7 +34,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "Subnet-AzureDevOps2"
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = azurerm_resource_group.rg.name
-  address_prefixes     = ["10.0.0.0/16"]
+  address_prefixes     = ["10.240.0.0/16"]
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
