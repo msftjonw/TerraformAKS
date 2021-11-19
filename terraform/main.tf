5910baa9186cfa53b1b6bd5de691b220f9ff4bdd
msftjonw <35560783+msftjonw@users.noreply.github.com>
2021-11-19 11:47:26 -0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,29 +19,29 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "RG-AzureDevOps2"
+  name     = "RG-AzureDevOps3"
   location = "West US 2"
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "VNet-AzureDevOps2"
+  name                = "VNet-AzureDevOps3"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/8"]
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "Subnet-AzureDevOps2"
+  name                 = "Subnet-AzureDevOps3"
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = azurerm_resource_group.rg.name
   address_prefixes     = ["10.240.0.0/16"]
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "AKS-AzureDevOps2"
+  name                = "AKS-AzureDevOps3"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "dns-azuredevops2"
+  dns_prefix          = "dns-azuredevops3"
 
   default_node_pool {
     name                = "systempool"
@@ -67,6 +67,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 
   tags = {
-    Environment = "azuredevops2"
+    Environment = "azuredevops3"
   }
 }
